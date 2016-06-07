2613de41bc3e17aa8b387afdf77fd969e8c7b068
zhanzhicai2 <1610893869@qq.com>
2016-06-07 23:55:35 +0800

    add

--- c.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get("http://www.maiziedu.com/")
-ele = browser.find_element_by_css_selector('img[alt = "UI设计"]')
-ele.click()
